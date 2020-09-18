a90df20aa2eb23c72c2e0c22cfae70e7c3fd3731
Pedro <pedroberorato@gmail.com>
2020-09-18 15:32:59 -0300

    First

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,6 @@ const app = express();
 
 app.use(routes);
 
-app.get('/', (request, response) => response.json({ message: 'Hello Barber' }));
+app.get('/', (request, response) => response.json({ message: 'Hello Barber 1234' }));
 
 app.listen(3333, () => console.log('Server Started!'));
