09e919f8852ab1e1c524d4ed1f1f2ab196d5b7c0
Pedro <pedroberorato@gmail.com>
2020-09-18 16:42:08 -0300

    Validate

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,10 @@
 import express from 'express';
 import routes from './routes';
-
+// Start Express
 const app = express();
-
+// Enable Json
+app.use(express.json());
+// Load Routes
 app.use(routes);
-
-app.get('/', (request, response) => response.json({ message: 'Hello Barber 1234' }));
-
+// Listener
 app.listen(3333, () => console.log('Server Started!'));
