a4f2692b25d1a24f0d3ec3e7fa456f39287ee278
Pedro <pedroberorato@gmail.com>
2020-09-18 22:43:44 -0300

    Repositories

---
+++ src/repositories/AppointmentsRepository.ts
@@ -0,0 +1,30 @@
+// Os Repositories realizam as operações dos dados
+import { isEqual } from 'date-fns';
+
+import Appointment from '../models/Appointment';
+
+class AppointmentsRepository {
+    private appointments: Appointment[] = [];
+
+    constructor() {
+        this.appointments = [];
+    }
+
+    public findByDate(date: Date): Appointment | null {
+        const findAppointment = this.appointments.find(item =>
+            isEqual(date, item.date),
+        );
+
+        return findAppointment || null;
+    }
+
+    public create(provider: string, date: Date): Appointment {
+        const appointment = new Appointment(provider, date);
+
+        this.appointments.push(appointment);
+
+        return appointment;
+    }
+}
+
+export default AppointmentsRepository;
