e5c38755c83cc60ae4f0c7c7fea5d4d2b9633a2c
Pedro <pedroberorato@gmail.com>
2020-09-21 17:20:36 -0300

    Typeorm

--- src/database/migrations/1600714937971-CreateAppointments.ts
+++ src/database/migrations/1600714937971-CreateAppointments.ts
@@ -12,6 +12,7 @@ export default class CreateAppointments1600714937971
                         type: 'varchar',
                         isPrimary: true,
                         generationStrategy: 'uuid',
+                        default: 'uuid_generate_v4()',
                     },
                     {
                         name: 'provider',
