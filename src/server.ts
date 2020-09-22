c610646877db89117d90ee72906ac485466f664a
Pedro <pedroberorato@gmail.com>
2020-09-22 20:09:20 -0300

    Rota de arquivos

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,15 @@
 import 'reflect-metadata';
 import express from 'express';
+
 import routes from './routes';
 import './database';
+import uploadConfig from './config/upload';
 // Start Express
 const app = express();
 // Enable Json
 app.use(express.json());
+// Rota para arquivos
+app.use('/files', express.static(uploadConfig.directory));
 // Load Routes
 app.use(routes);
 // Listener
