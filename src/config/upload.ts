b521c3281c6af58a899e7df4aa284356cde6743b
Pedro <pedroberorato@gmail.com>
2020-09-22 20:04:31 -0300

    Update Avatar

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -2,14 +2,18 @@ import path from 'path';
 import multer from 'multer';
 import crypto from 'crypto';
 
+const tmpFolder = path.resolve(__dirname, '..', '..', 'tmp');
+
 export default {
+    directory: tmpFolder,
+
     storage: multer.diskStorage({
-        destination: path.resolve(__dirname, '..', '..', 'tmp'),
+        destination: tmpFolder,
         filename(request, file, callback) {
             const fileHash = crypto.randomBytes(10).toString('hex');
             const fileName = `${fileHash}-${file.originalname}`;
 
             return callback(null, fileName);
-        }
+        },
     }),
 };
