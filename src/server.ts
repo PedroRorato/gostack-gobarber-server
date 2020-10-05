4e03976208c75f5dbbd9285af1f43ef1a361228c
Pedro <pedroberorato@gmail.com>
2020-10-05 14:20:56 -0300

    Cors

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,37 @@
 import 'reflect-metadata';
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
+import cors from 'cors';
+import 'express-async-errors';
 
 import routes from './routes';
 import './database';
 import uploadConfig from './config/upload';
+import AppError from './errors/AppError';
 // Start Express
 const app = express();
+// Habilita solicitações de outros servidores
+app.use(cors());
 // Enable Json
 app.use(express.json());
 // Rota para arquivos
 app.use('/files', express.static(uploadConfig.directory));
 // Load Routes
 app.use(routes);
+// Middleware para tratar erros
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+    if (err instanceof AppError) {
+        return response.status(err.statusCode).json({
+            status: 'error',
+            message: err.message,
+        });
+    }
+
+    console.error(err);
+
+    return response.status(500).json({
+        status: 'error',
+        message: 'Internal server error',
+    });
+});
 // Listener
 app.listen(3333, () => console.log('Server Started!'));
